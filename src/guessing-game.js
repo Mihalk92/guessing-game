7eb10ccd4a5c0ae79663b82cf1bfb17055ce797e
Dmitry Kovalenko <mihalk9292@gmail.com>
2023-09-07 10:39:41 +0300

    init: start task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,5 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    
 }
 
 module.exports = GuessingGame;
