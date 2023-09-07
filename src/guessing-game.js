ef8e158fe01ecb79f86d5670f7e04393e1f170aa
Dmitry Kovalenko <mihalk9292@gmail.com>
2023-09-07 10:40:24 +0300

    feat: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,5 +1,27 @@
 class GuessingGame {
-    
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
+
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        this.carrent = 0;
+    }
+
+    guess() {
+        this.carrent = Math.ceil((this.max - this.min) / 2 + this.min)
+        return this.carrent;
+    }
+
+    lower() {
+        this.max = this.carrent
+    }
+
+    greater() {
+        this.min = this.carrent
+    }
 }
 
 module.exports = GuessingGame;
